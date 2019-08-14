49aea96e74813cfe95b78a4286c08e598085a604
Sil van Diepen <me@silvandiepen.com>
2019-08-14 12:05:27 +0200

    add loader and new files from git

--- saofile.js
+++ saofile.js
@@ -67,52 +67,154 @@ module.exports = {
   ],
   async completed() {
     this.gitInit()
-    var nuxt = await exec('(curl -s0 https://raw.githubusercontent.com/henris-style/henris-app-files/master/nuxt/setup.sh) | bash', function(err, stdout) {
-      if (err) {
-        console.log("Couldn\'t setup Nuxt files\n\n", err)
-      }
-      console.log(stdout)
-    })
-    var assets = await exec('mkdir -p assets/scss && cd assets/scss && (curl -s0 https://raw.githubusercontent.com/henris-style/setup-files/master/setup.sh) | bash', function(err, stdout) {
-      if (err) {
-        console.log("couldn't create all Henri's files\n\n", err)
-      }
-      console.log(stdout)
-    })
-    
-    await this.npmInstall()
 
     console.log()
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhhhhhhhhhhe  .hhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhhhhhhhhhh   ehhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hh    hhhhhhh    hhhhe      '))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhh      hh   hhhh   hhhhh    sss+'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhh    hhhhi   hhhhhhhhhhh   ssshh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhh   hhhhhhhhhhhy      h'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhh   hhhhhhhhhhhhhhsss  '))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhh   hhhhhhhhhhe  hhhh  '))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhh   hhhhhhhhhhhs       '))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhsssh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log(this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'))
-    console.log()
-    console.log(this.chalk.bold(`\tYou are done! Happy developing.\n`))
-    console.log()
-    console.log(`\tGo to your project - cd ${this._answers.project_name}`)
-    console.log(`\tRun ${this.chalk.bold('\'npm run dev\'')} do get started..`)
-    console.log()
+
+    var loader = []
+    let timer = 250
+    var x = 0
+
+    const doLoader = () => {
+      if (x < 50) {
+        loader.push('h')
+        x++
+        process.stdout.write('\r' + this.chalk.blue(loader.join('')))
+      } else {
+        clearInterval(loadTimer)
+      }
+    }
+    let loadTimer = (() => {
+      return setInterval(() => {
+        doLoader()
+      }, timer)
+    })()
+
+    const nuxt = await exec(
+      `(curl -s0 https://raw.githubusercontent.com/henris-style/henris-app-files/master/nuxt/setup.sh) | bash -s ${this.outFolder}`,
+      function(err, stdout) {
+        if (err) {
+          console.log("Couldn't setup Nuxt files\n\n", err)
+        }
+        console.log("Created Henri's style Files", stdout)
+      }
+    )
+
+    const guyn = this._answers.project_options.includes('guyn')
+      ? 'guyn'
+      : 'no-guyn'
+
+    const assets = await exec(
+      `(curl -s0 https://raw.githubusercontent.com/henris-style/setup-files/master/setup.sh) | bash -s ${this.outFolder}/assets/scss ${this.guyn}`,
+      function(err, stdout) {
+        if (err) {
+          console.log("couldn't create all Henri's files\n\n", err)
+        } else {
+          console.log('Created Nuxt Files', stdout)
+        }
+      }
+    )
+
+    clearInterval(loadTimer)
+    loadTimer = (() => {
+      return setInterval(() => {
+        doLoader()
+      }, 10)
+    })()
+
+    // await this.npmInstall()
+    setTimeout(() => {
+      console.log(
+        this.chalk.blue('\nhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhhhhhhhhhhe  .hhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhhhhhhhhhh   ehhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hh    hhhhhhh    hhhhe      ')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhh      hh   hhhh   hhhhh    ssss')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhh    hhhhi   hhhhhhhhhhh   ssshh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhh   hhhhhhhhhhhy      h')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhh   hhhhhhhhhhhhhhsss  ')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhh   hhhhhhhhhhe  hhhh  ')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhh   hhhhhh   hhhhhhhhhhhs       ')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhsssh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log(
+        this.chalk.blue('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh')
+      )
+      console.log()
+      console.log()
+      console.log(
+        this.chalk.bold(`   Done! Your `) +
+          this.chalk.bold.green(`Nuxt `) +
+          this.chalk.bold.blue(`Henri's`) +
+          this.chalk.bold(` project is ready.\n`)
+      )
+      console.log()
+      console.log(
+        `   Go to your project - cd ${this.chalk.bold(
+          this._answers.project_name
+        )}`
+      )
+      console.log(`   Run ${this.chalk.bold("'npm run dev'")} to get started..`)
+      console.log()
+      console.log()
+    }, 1000)
   }
 }
